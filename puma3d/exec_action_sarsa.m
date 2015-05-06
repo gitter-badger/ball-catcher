b930bfa48933eba98577a76e8a82786d3b968bf0
Sabyasachi Shivkumar <sabyashiv@gmail.com>
2015-05-06 11:08:46 +0530

    Added control using arms

--- puma3d/exec_action_sarsa.m
+++ puma3d/exec_action_sarsa.m
@@ -1,5 +1,5 @@
 function [feat,a]=exec_action_sarsa(sp,ap)
-alp=0.5;
+alp=1;
 act=[[zeros(4,1);ones(4,1)],repmat([zeros(2,1);ones(2,1)],2,1),repmat([zeros(1,1);ones(1,1)],4,1)];
 balls = getappdata(0, 'balls');
 if nargin==0
@@ -33,7 +33,19 @@ new_pos=arm_tip'+alp*(tmp.*act(a,:)');
 %if new_pos(3)>1401
  %   new_pos(3)=1200;
 %end
+
+
+
 [theta1,theta2,theta3,theta4,theta5,theta6] = arm_IK(new_pos(1),new_pos(2),new_pos(3));
+
+arm = getappdata(0, 'arm');
+t1=arm.theta(1);
+t2=arm.theta(2);
+t3=arm.theta(3);
+
+t1d=t1+alp*(theta1-t1);
+t2d=t2+alp*(theta2-t2);
+t3d=t3+alp*(theta3-t3);
 
 %{
 global exec_rew
@@ -50,6 +62,6 @@ end
 n = 2;    % demo ani steps
 
 %if nogo~=1
-arm_animate(theta1, theta2, theta3-180, 0, 0, 0, n, 'y')
+arm_animate(t1d, t2d, t3d, 0, 0, 0, n, 'y')
 %end
 end
