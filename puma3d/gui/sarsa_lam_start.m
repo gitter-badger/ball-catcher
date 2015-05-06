0804856c6ba923ea3fde637e432efb76f98dbd80
AbdealiJK <abdealikothari@gmail.com>
2015-05-06 11:35:40 +0530

    Add report

--- puma3d/gui/sarsa_lam_start.m
+++ puma3d/gui/sarsa_lam_start.m
@@ -1,18 +1,17 @@
 function sarsa_lam_start(h, dummy)
-load thet_sarsa
+    load thet_sarsa
 
+    if getappdata(0, 'learning_sarsa')==0
+    setappdata(0, 'learning_sarsa',1);
+    else
+    setappdata(0, 'learning_sarsa',0);
+    end
+    setappdata(0, 'thet_sarsa', thet);
+    for i=1:10
 
-if getappdata(0, 'learning_sarsa')==0
-setappdata(0, 'learning_sarsa',1);
-else
-setappdata(0, 'learning_sarsa',0);
-end
-setappdata(0, 'thet_sarsa', thet);
-for i=1:10
-
-    create_ball_button_press;
-    ball_move_down;
-end
-thet = getappdata(0, 'thet_sarsa');
-save(cat(2,pwd,'/thet_sarsa'),'thet');
+        create_ball_button_press;
+        ball_move_down;
+    end
+    thet = getappdata(0, 'thet_sarsa');
+    save(cat(2,pwd,'/thet_sarsa'),'thet');
 end
